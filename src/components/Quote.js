a8f6283749e8bfc29fdf8da71342fb81d313a64a
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-07-22 21:00:15 -0400

    First commit

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Quote = () => {
+  return (
+    <View>
+      <Text>Quote</Text>
+    </View>
+  )
+}
+
+export default Quote
+
+const styles = StyleSheet.create({})
